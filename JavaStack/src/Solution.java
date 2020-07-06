2845181b95a49f53f1afbb34976cba4e58fbe166
dreagon19 <anjit193023@gmail.com>
2020-07-06 23:06:53 +0530

    HackerRank_Solution 1

--- JavaStack/src/Solution.java
+++ JavaStack/src/Solution.java
@@ -3,6 +3,6 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
-        System.out.println("JJJJJJ");
+        System.out.println("JJJJJ");
     }
 }
